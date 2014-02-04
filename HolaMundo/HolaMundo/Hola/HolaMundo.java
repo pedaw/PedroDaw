5f7bb75f0e23bafa0fdf6fa7cde7dc1433c066bf
usuario <usuario@10.0.2.15>
2014-02-04 18:34:52 +0100

    tercer commit

--- HolaMundo/HolaMundo/Hola/HolaMundo.java
+++ HolaMundo/HolaMundo/Hola/HolaMundo.java
@@ -8,6 +8,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo Pedro");
+		System.out.println("esto es una modificacion");
 	}
 
 }
